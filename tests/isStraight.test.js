3aeba37f3c9fed8c753e6fc5b57c48f995846b82
dmikozzz <146703887+dmikozzz@users.noreply.github.com>
2024-10-15 09:48:39 +0200

    update
    test

--- tests/isStraight.test.js
+++ tests/isStraight.test.js
@@ -8,3 +8,9 @@ test('check that isStraight returns truthy if straight', () => {
   let hand = new Hand('♥9', '♦8', '♣7', '♥5', '♦6');
   expect(CompareHands.isStraight(hand)).toBeTruthy();
 });
+
+test('compare two straights, higher one wins', () => {
+  let hand1 = new Hand('♥9', '♦8', '♣7', '♠6', '♦5');
+  let hand2 = new Hand('♥A', '♦K', '♣Q', '♠J', '♦T');
+  expect(CompareHands.isStraight(hand2)).toBeGreaterThan(CompareHands.isStraight(hand1));
+});
